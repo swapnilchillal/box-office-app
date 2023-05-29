f1ff4106af69ef9ff0799be2b7770926348491be
Swapnil Chillalnode <srchillal589@gmail.com>
2023-05-29 23:47:16 +0530

    installed react-router-dom to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Starred from "./Starred";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<div> Contacting </div>} />
+        <Route path="/starred" element={<Starred />} />
+        <Route path="*" element={"Hellwo Error"} />
+
+        {/* <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+      </Route>
+      <Route element={<PageLayout />}>
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/tos" element={<Tos />} />
+      </Route>
+      <Route path="contact-us" element={<Contact />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
