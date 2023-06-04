2d3391c2d7a35516717bd8d9bb4bb9c6dbf888cb
Swapnil Chillalnode <srchillal589@gmail.com>
2023-06-04 22:39:04 +0530

    replaced few lines with useQuery in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,43 @@
 import { useState } from 'react';
-import { searchForShows, searchForPeople } from './../api/tvmaze';
+// import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+import { useQuery } from '@tanstack/react-query';
+import { searchForPeople, searchForShows } from '../api/tvmaze';
 
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  // const [apiData, setApiData] = useState(null);
+  // const [apiDataError, setApiDataError] = useState(null);
+
+  const [filter, setFilter] = useState(null);
+
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () =>
+      filter.searchOption === 'shows'
+        ? searchForShows(filter.searchStr)
+        : searchForPeople(filter.searchStr),
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
 
   const onSearch = async ({ searchStr, searchOption }) => {
-    try {
-      setApiDataError(null);
+    setFilter({ searchStr, searchOption });
 
-      let result;
-
-      if (searchOption === 'shows') {
-        result = await searchForShows(searchStr);
-      } else {
-        result = await searchForPeople(searchStr);
-      }
-      if (!result[0]) setApiDataError(Error("No results found")); //To Check is array is empty or undefined
-      else setApiData(result);
-    } catch (error) {
-      setApiDataError(error);
-    }
+    // try {
+    //   setApiDataError(null);
+    //   let result;
+    //   if (searchOption === 'shows') {
+    //     result = await searchForShows(searchStr);
+    //   } else {
+    //     result = await searchForPeople(searchStr);
+    //   }
+    //   if (!result[0]) setApiDataError(Error("No results found")); //To Check is array is empty or undefined
+    //   else setApiData(result);
+    // } catch (error) {
+    //   setApiDataError(error);
+    // }
   };
 
   const renderApiData = () => {
@@ -32,11 +46,12 @@ const Home = () => {
     }
 
     if (apiData) {
-      return apiData[0].show
-        ? <ShowGrid shows={apiData}/>
-        : <ActorsGrid actors={apiData} />
-          
-    };
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorsGrid actors={apiData} />
+      );
+    }
 
     return null;
   };
